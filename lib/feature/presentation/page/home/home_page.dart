f039b569f03f7bd9c10ef7c690157976b521b2b0
shindonghwi <ehdgnl8940@naver.com>
2022-09-16 18:20:44 +0900

    feat: makeMenu widget 생성

--- lib/feature/presentation/page/home/home_page.dart
+++ lib/feature/presentation/page/home/home_page.dart
@@ -1,49 +1,70 @@
+import 'dart:developer';
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Map<String, dynamic> menuList = {
+    'BottomNavigation': ['Basic', 'Shifting'],
+  }.map((key, value) => MapEntry(key, value));
+
+  List<bool> subMenuHiddenState = <bool>[true];
+
+  Widget makeMenu() {
+
+    return Column(
+      children: [
+        ListTile(
+          title: Text('Bottom Navigation'),
+          onTap: () {
+            // subMenuHiddenState[menuList.indexOf(title)] = !subMenuHiddenState[menuList.indexOf(title)];
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Bottom Navigation')));
+          },
+          // leading: leadingIcon,
+          // trailing: trailingIcon,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Material Sample App'),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          tooltip: 'menu',
-          onPressed: () {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Click Menu')));
-          },
+        appBar: AppBar(
+          title: const Text('Material Sample App'),
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: 'menu',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Click Menu')));
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              tooltip: 'search',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Click Search')));
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.add_alert),
+              tooltip: 'alert',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Click Alert')));
+              },
+            ),
+          ],
         ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            tooltip: 'search',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Click Search')));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'alert',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Click Alert')));
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        children: [],
-      ),
+        body: makeMenu()
     );
   }
 }
