7a637d6b0389ead33ccf4c2863811dfde0e5fe36
shindonghwi <ehdgnl8940@naver.com>
2022-09-17 20:52:01 +0900

    feat: 대댓글과 유사한 목록 UI 구현

--- lib/feature/presentation/page/home/home_page.dart
+++ lib/feature/presentation/page/home/home_page.dart
@@ -1,7 +1,3 @@
-import 'dart:developer';
-import 'dart:ffi';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,26 +9,74 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Map<String, dynamic> menuList = {
-    'BottomNavigation': ['Basic', 'Shifting'],
+    'Bottom Navigation': ['Basic', 'Shifting'],
+    'Bottom Sheet': ['Basic', 'List'],
+    'Side Sheet': ['Basic'],
+    'Buttons': ['Basic', 'Button In Utilities'],
+    'Backdrop': ['Basic', 'Filter', 'Navigation', 'Steppers', 'Text Field', 'Selection']
   }.map((key, value) => MapEntry(key, value));
 
-  List<bool> subMenuHiddenState = <bool>[true];
+  final Map<String, dynamic> _selected = {}; // 좋아요 상태
 
-  Widget makeMenu() {
+  @override
+  void initState() {
+    super.initState();
+    initSelectedState();
+  }
 
-    return Column(
-      children: [
-        ListTile(
-          title: Text('Bottom Navigation'),
-          onTap: () {
-            // subMenuHiddenState[menuList.indexOf(title)] = !subMenuHiddenState[menuList.indexOf(title)];
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Bottom Navigation')));
-          },
-          // leading: leadingIcon,
-          // trailing: trailingIcon,
-        ),
-      ],
-    );
+  /// 좋아요 클릭 상태 초기화
+  void initSelectedState() {
+    for (var element in menuList.entries) {
+      List<bool> likeState = [];
+      element.value.toList().forEach((element) {
+        likeState.add(false);
+      });
+      _selected[element.key] = likeState;
+    }
+  }
+
+  Widget makeMenuList() {
+    return ListView.builder(
+        itemCount: menuList.length,
+        itemBuilder: (BuildContext context, int idx) {
+          String key = menuList.keys.elementAt(idx);
+          List<String> values = menuList.values.elementAt(idx);
+          List<Widget> subList = [];
+
+          values.asMap().forEach((index, subMenu) {
+            subList.add(
+              Padding(
+                padding: const EdgeInsets.only(left: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      subMenu,
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _selected[key][index] = !_selected[key][index];
+                          });
+                        },
+                        icon: Icon(
+                          _selected[key][index] == true ? Icons.favorite : Icons.favorite_outline,
+                          color: Colors.pink,
+                        ))
+                  ],
+                ),
+              ),
+            );
+          });
+
+          return ExpansionTile(
+              title: Text(key.toString()),
+              subtitle: Text('count: ${values.length}'),
+              initiallyExpanded: false,
+              controlAffinity: ListTileControlAffinity.platform,
+              children: subList);
+        });
   }
 
   @override
@@ -64,7 +108,6 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: makeMenu()
-    );
+        body: makeMenuList());
   }
 }
