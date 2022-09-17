157344e07aa0438ddd6031300213cdf8a2328ce0
shindonghwi <ehdgnl8940@naver.com>
2022-09-17 21:21:49 +0900

    feat: menu list 별도 파일에 분리

---
+++ lib/feature/presentation/page/home/model/menu_list.dart
@@ -0,0 +1,145 @@
+Map<String, dynamic> menuList = {
+  // 1
+  'Bottom Navigation': [
+    'Basic',
+    'Shifting',
+    'Light',
+    'Dark',
+    'Icon',
+    'Primary',
+    'Map Blue',
+    'Light Sample',
+    'Article',
+    'Shop',
+    'Small',
+    'Main',
+    'Badge',
+    'Badge Blink',
+    'Animated'
+  ],
+
+  // 2
+  'Bottom Sheet': [
+    'Basic',
+    'List',
+    'Map',
+    'Floating',
+    'Full',
+    'Filter',
+    'Menu',
+    'Player',
+    'Expand',
+    'Animation',
+    'Transform'
+  ],
+
+  // 3
+  'Side Sheet': ['Basic'],
+
+  // 4
+  'Buttons': [
+    'Basic',
+    'Button In Utilities',
+    'Fab Middle',
+    'Fab More',
+    'Fab More Text',
+    'Toggle Basic',
+    'Fab Extend',
+    'Material',
+    'High Emphasis',
+    'Outlined',
+    'Text Label',
+    'With Icon',
+    'Fab Animation',
+    'Fab Expand',
+  ],
+
+  // 5
+  'Backdrop': [
+    'Basic',
+    'Filter',
+    'Navigation',
+    'Steppers',
+    'Text Field',
+    'Selection'
+  ],
+
+  // 6
+  'Cards': [
+    'Basic',
+    'Timeline',
+    'Overlap',
+    'Wizard',
+    'Wizard Light',
+    'Wizard Overlap',
+    'Outlined',
+    'Divider',
+    'Expand',
+    'Scrolling',
+  ],
+
+  // 7
+  'Chips': [
+    'Basic',
+    'Tag',
+    'Group',
+    'Input',
+    'Choice',
+    'Filter',
+    'Action',
+  ],
+
+  // 8
+  'Check Radio': [
+    'CheckBox Basic',
+    'Radio Switch',
+    'CheckBox Parent Child',
+  ],
+
+  // 9
+  'Dialogs': [
+    'Basic',
+    'Fullscreen',
+    'Custom',
+    'Custom Info',
+    'Custom Warning',
+    'Custom Light',
+    'Custom Dark',
+    'Custom Add Post',
+    'Custom Add Review',
+    'GDPR Basic',
+    'Term of Services',
+    'Header',
+    'Image',
+    'Menu Payment',
+    'Achievement',
+    'Contact Us',
+  ],
+
+  // 10
+  'Motion': [
+    'Page Basic',
+    'Card',
+    'FAB',
+    'List',
+    'Search Bar Expand',
+    'Fab View',
+  ],
+
+  // 11
+  'Expansion Panels': [
+    'Basic',
+    'Invoice',
+    'Ticket',
+  ],
+
+  // 12
+  'Grid': [
+    'Basic',
+    'Single Line',
+    'Two Line',
+    'Sectioned',
+    'Albums',
+    'Caller',
+  ],
+}.map((key, value) => MapEntry(key, value));
